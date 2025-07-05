582fbdc04e89cce0d0a2afdb002f46d41327dfaf
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-05 15:56:18 +0000

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,9 @@ function App(){
     <>
       <main className='min-h-full min-w-[375px] lg:w-[1440px] overflow-y-auto bg-transparent font-poppins flex flex-col items-center justify-center'>
         <header className='w-full lg:w-[500px] text-center px-8 space-y-2 my-6'>
-          <h1 className='font-light text-xl text-gray-400'>Reliable, efficient delivery</h1>
-          <h1 className='font-[600] text-xl text-gray-500'>Powered by Technology</h1>
-          <h1 className='text-sm text-gray-400'>Our Artificial Intelligence powered tools inspire millions of projects accross the globe. Feel free to explore them.</h1>
+          <h2 className='font-[100] text-2xl text-gray-400'>Reliable, efficient delivery</h2>
+          <h1 className='font-[600] text-2xl text-gray-500'>Powered by Technology</h1>
+          <h3 className='text-sm text-gray-400'>Our Artificial Intelligence powered tools inspire millions of projects accross the globe. Feel free to explore them.</h3>
         </header>
         <article className='grid p-8 grid-cols-1 lg:grid-flow-col lg:grid-rows-4 gap-6 lg:w-[900px]'>
           <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-red lg:row-span-2 lg:ml-[350px] lg:row-start-1'>
