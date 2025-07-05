351769197e68898995652a69a23753f504454cae
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-05 04:04:08 +0000

    declare all custom colors

--- four-card-feature-section/src/App.jsx
+++ four-card-feature-section/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import './index.css'
 
 function App(){
   return (
     <>
-      <main className='min-h-full w-[375px] lg:w-[1440px] bg-fuchsia-400'>
+      <main className='min-h-full w-[375px] lg:w-[1440px] bg-red font-poppins'>
         Hello world
       </main>
     </>
