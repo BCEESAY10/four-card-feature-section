c0aef30d4830a56c048b625c05f5401e2d955f67
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-05 06:11:50 +0000

    allow vertical overflow

--- four-card-feature-section/src/App.jsx
+++ four-card-feature-section/src/App.jsx
@@ -4,7 +4,7 @@ function App(){
 
   return (
     <>
-      <main className='min-h-full min-w-[375px] lg:w-[1440px] bg-transparent p-6 lg:p-16 font-poppins flex flex-col items-center justify-center'>
+      <main className='min-h-full min-w-[375px] lg:w-[1440px] overflow-y-auto bg-transparent p-6 lg:p-16 font-poppins flex flex-col items-center justify-center'>
         <header className='w-full lg:w-[500px] text-center px-8 space-y-8'>
           <h1 className='font-light text-xl text-gray-400'>Reliable, efficient delivery</h1>
           <h1 className='font-[600] text-xl text-gray-500'>Powered by Technology</h1>
