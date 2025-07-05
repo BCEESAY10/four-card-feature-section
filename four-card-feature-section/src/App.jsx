c02855c37fd06eff29ad00691bfa945be0541d8e
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-05 05:57:30 +0000

    adjust heights and widths

--- four-card-feature-section/src/App.jsx
+++ four-card-feature-section/src/App.jsx
@@ -10,29 +10,29 @@ function App(){
           <h1 className='font-[600] text-xl text-gray-500'>Powered by Technology</h1>
           <h1 className='text-sm text-gray-400'>Our Artificial Intelligence powered tools inspire millions of projects accross the globe. Feel free to explore them.</h1>
         </header>
-        <article className='grid px-8 gap-6 mt-10 grid-cols-1 lg:grid-cols-3 lg:grid-rows-3 lg:w-[900px]'>
-          <div className='w-[90%] lg:w-46 h-42 lg:h-36 relative bg-white p-6 rounded-lg shadow-lg border-t-[6px] border-red lg:row-start-1 lg:col-start-2'>
+        <article className='grid px-8 gap-6 mt-10 grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 lg:w-[900px]'>
+          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-lg border-t-[6px] border-red lg:row-start-1 lg:col-start-2'>
              <h4 className='font-[600] text-gray-500'>Team Builder</h4>
              <p className='text-sm text-gray-400'>Scans and see how to scout new members to for us to grow</p>
-             <img className='absolute h-8 w-8 bottom-4 right-4' src="/images/icon-team-builder.svg" alt="Team builder image" />
+             <img className='absolute h-10 w-10 bottom-4 right-4' src="/images/icon-team-builder.svg" alt="Team builder image" />
           </div>
 
-          <div className='w-[90%] lg:w-46 h-42 lg:h-36 relative bg-white p-6 rounded-lg shadow-lg border-t-[6px] border-cyan lg:row-start-2'>
+          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-lg border-t-[6px] border-cyan lg:row-start-2 lg:col-start-1'>
              <h4 className='font-[600] text-gray-500'>Supervisor</h4>
              <p className='text-sm text-gray-400'>Monitors activity to identify project roadblocks</p>
-             <img className='absolute h-8 w-8 bottom-4 right-4' src="/images/icon-supervisor.svg" alt="Team builder image" />
+             <img className='absolute h-10 w-10 bottom-4 right-4' src="/images/icon-supervisor.svg" alt="Team builder image" />
           </div>
 
-          <div className='w-[90%] lg:w-46 h-42 lg:h-36 relative bg-white p-6 rounded-lg shadow-lg border-t-[6px] border-orange lg:row-start-2 lg:col-start-2'>
+          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-lg border-t-[6px] border-orange lg:row-start-2 lg:col-start-2'>
              <h4 className='font-[600] text-gray-500'>Karma</h4>
              <p className='text-sm text-gray-400'>Regularly evaluates our talent to ensure quality</p>
-             <img className='absolute h-8 w-8 bottom-4 right-4' src="/images/icon-karma.svg" alt="Team builder image" />
+             <img className='absolute h-10 w-10 bottom-4 right-4' src="/images/icon-karma.svg" alt="Team builder image" />
           </div>
 
-          <div className='w-[90%] lg:w-46 h-42 lg:h-36 relative bg-white p-6 rounded-lg shadow-lg border-t-[6px] border-blue lg:row-start-2 lg:col-start-3'>
+          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-lg border-t-[6px] border-blue lg:row-start-2 lg:col-start-3'>
              <h4 className='font-[600] text-gray-500'>Calculator</h4>
              <p className='text-sm text-gray-400'>Uses data from past projects to provide better delivery estimates</p>
-             <img className='absolute h-8 w-8 bottom-4 right-4' src="/images/icon-calculator.svg" alt="Team builder image" />
+             <img className='absolute h-10 w-10 bottom-4 right-4' src="/images/icon-calculator.svg" alt="Team builder image" />
           </div>
         </article>
       </main>
