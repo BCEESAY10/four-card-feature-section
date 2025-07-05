ffeeda5f36009e036994bfdf06d76d44eccfc70e
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-05 04:33:21 +0000

    Add the header

--- four-card-feature-section/src/App.jsx
+++ four-card-feature-section/src/App.jsx
@@ -4,8 +4,12 @@ import './index.css'
 function App(){
   return (
     <>
-      <main className='min-h-full w-[375px] lg:w-[1440px] bg-red font-poppins'>
-        Hello world
+      <main className='min-h-full min-w-[375px] lg:w-[1440px] bg-transparent p-0 lg:p-16 font-poppins flex flex-col items-center justify-start'>
+        <header className='w-full lg:w-1/4 text-center'>
+          <h1 className='font-[200] text-xl text-gray-400'>Reliable, efficient delivery</h1>
+          <h1 className='font-[600] text-xl text-gray-500'>Powered by Technology</h1>
+          <h1 className='text-sm text-gray-400'>Our Artificial Intelligence powered tools inspire millions of projects accross the globe. Feel free to explore them.</h1>
+        </header>
       </main>
     </>
   )
