72c706a3d2b479cd13553901750522c05d83d600
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-05 03:16:38 +0000

    First commit

---
+++ four-card-feature-section/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App(){
+  return (
+    <>
+      <main className='min-h-full w-[375px] lg:w-[1440px] bg-fuchsia-400'>
+        Hello world
+      </main>
+    </>
+  )
+}
+
+export default App
