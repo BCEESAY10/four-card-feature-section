a5d540a5700c4b24f42c05a14902a71a0e306be6
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-06 03:27:17 +0000

    remove unwanted margin left

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App(){
           <h3 className='text-sm text-gray-400'>Our Artificial Intelligence powered tools inspire millions of projects accross the globe. Feel free to explore them.</h3>
         </header>
         <article className='grid p-8 lg:p-0 grid-cols-1 lg:grid-cols-3 lg:grid-rows-4 gap-6 lg:w-[1000px] border border-red'>
-          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-red lg:col-start-1 lg:ml-[350px] lg:row-start-1'>
+          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-red lg:col-start-2 lg:row-start-1'>
              <h4 className='font-[600] text-gray-500'>Team Builder</h4>
              <p className='text-sm text-gray-400'>Scans our talent network to create the optimal team for your project</p>
              <img className='absolute h-10 w-10 bottom-4 right-4' src="images/icon-team-builder.svg" alt="Team builder image" />
