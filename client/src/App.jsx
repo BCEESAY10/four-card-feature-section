9db7a7ebd47aa819196b9f9b1c876dcf60961ecc
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-06 03:24:43 +0000

    properly position all grid items

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,26 +10,26 @@ function App(){
           <h1 className='font-[600] text-2xl text-gray-500'>Powered by Technology</h1>
           <h3 className='text-sm text-gray-400'>Our Artificial Intelligence powered tools inspire millions of projects accross the globe. Feel free to explore them.</h3>
         </header>
-        <article className='grid p-8 lg:p-0 grid-cols-1 lg:grid-flow-col lg:grid-rows-4 gap-6 lg:w-[1000px] border border-red'>
-          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-red lg:row-span-2 lg:ml-[350px] lg:row-start-1'>
+        <article className='grid p-8 lg:p-0 grid-cols-1 lg:grid-cols-3 lg:grid-rows-4 gap-6 lg:w-[1000px] border border-red'>
+          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-red lg:col-start-1 lg:ml-[350px] lg:row-start-1'>
              <h4 className='font-[600] text-gray-500'>Team Builder</h4>
              <p className='text-sm text-gray-400'>Scans our talent network to create the optimal team for your project</p>
              <img className='absolute h-10 w-10 bottom-4 right-4' src="images/icon-team-builder.svg" alt="Team builder image" />
           </div>
 
-          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-cyan lg:row-span-2 lg:row-start-2'>
+          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-cyan lg:col-start-1 lg:row-start-2'>
              <h4 className='font-[600] text-gray-500'>Supervisor</h4>
              <p className='text-sm text-gray-400'>Monitors activity to identify project roadblocks</p>
              <img className='absolute h-10 w-10 bottom-4 right-4' src="/images/icon-supervisor.svg" alt="Team builder image" />
           </div>
 
-          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-orange lg:row-span-2 lg:row-start-3'>
+          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-orange lg:col-start-2 lg:row-start-3'>
              <h4 className='font-[600] text-gray-500'>Karma</h4>
              <p className='text-sm text-gray-400'>Regularly evaluates our talent to ensure quality</p>
              <img className='absolute h-10 w-10 bottom-4 right-4' src="/images/icon-karma.svg" alt="Team builder image" />
           </div>
 
-          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-blue lg:row-span-2 lg:row-start-2'>
+          <div className='w-[300px] min-h-[200px] relative bg-white p-6 rounded-lg shadow-md border-t-[6px] border-blue lg:col-start-3 lg:row-start-2'>
              <h4 className='font-[600] text-gray-500'>Calculator</h4>
              <p className='text-sm text-gray-400'>Uses data from past projects to provide better delivery estimates</p>
              <img className='absolute h-10 w-10 bottom-4 right-4' src="/images/icon-calculator.svg" alt="Team builder image" />
